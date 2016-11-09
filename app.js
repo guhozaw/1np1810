c31117b15312b12fe421b8df2b4d76c9646eb141
CloudAppDev <CloudAppDev@WIN-0B5HA0VMEOQ>
2016-11-09 15:45:54 +0400

    End!

--- app.js
+++ app.js
@@ -5,15 +5,17 @@ var options = {
 	host : 'www.nasa.gov',
 	path : '/sites/default/files/thumbnails/image/viirs_9apr2015.jpg'
 };
-//var options = {
-//	host : 'newsapi.org',
-//	path : '/v1/articles?source=cnn&apiKey=0ecb50865d59472f92fd558bdfa4fd2d'
-//};
+// var options = {
+// host : 'newsapi.org',
+// path : '/v1/articles?source=cnn&apiKey=0ecb50865d59472f92fd558bdfa4fd2d'
+// };
 var http = require('http');
 var https = require('https');
 function handleRequest(request, response) {
-	response.writeHead(200, {'Content-Type': 'text/plain' });
-	response.write('Getting content');
+	response.writeHead(200, {
+		'Content-Type' : 'text/plain'
+	});
+	response.write('Getting content\n');
 	var res = https.get(options, function(res) {
 		var pageData = "";
 		res.on('data', function(chunk) {
